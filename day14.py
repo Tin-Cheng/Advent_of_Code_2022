93547d5491ab127a57d2b4b18afaa611ed24ee76
TinCheng-CM <73582350+TinCheng-CM@users.noreply.github.com>
2022-12-14 14:33:47 +0800

    day14

---
+++ day14.py
@@ -0,0 +1,77 @@
+import time
+#f = open("day14test.txt", "r")
+f = open("day14input.txt", "r")
+s = f.read().splitlines()
+Lines = []
+maxX,maxY = float('-inf'),float('-inf')
+
+for str in s:
+    L = str.split(' -> ')
+    line = []
+    for point in L:
+        x,y = point.split(',')
+        x,y = int(x),int(y)
+        maxX,maxY = max(maxX,x),max(maxY,y)
+        line.append([x,y])
+    Lines.append(line)
+maxX = maxX + 2 + maxY
+maxY = maxY + 2
+empty = ' '
+M = [[empty] * (maxX) for _ in range(maxY)]
+for line in Lines:
+    lastX,lastY = None, None
+    for x,y in line:
+        if lastX:
+            if lastX == x:
+                dy = 1 if y - lastY > 0 else -1
+                while lastY != y:
+                    lastY += dy
+                    M[lastY][x] = '#'
+            else:
+                dx = 1 if x - lastX > 0 else -1
+                while lastX != x:
+                    lastX += dx
+                    M[y][lastX] = '#'
+        M[y][x] = '#'
+        lastX,lastY = x,y 
+initX = 500
+initY = 0
+draw = False
+def dropSand(part) -> bool:
+    if draw:
+        time.sleep(0.3)
+        for i in range(20):
+            print()
+        for m in M:
+            print(m[480:520])
+    curX = initX
+    curY = initY
+    while curY < maxY - 1:
+        if M[curY + 1][curX] == empty:
+            curY += 1
+        elif 0 <= curX - 1 and M[curY + 1][curX - 1] == empty:
+            curX -= 1
+            curY += 1
+        elif curX + 1 < maxX and M[curY + 1][curX + 1] == empty:
+            curX += 1
+            curY += 1
+        else: break
+    if part == 1 and curY == maxY - 1: return False
+    elif part == 2 and curX == initX and curY == initY: return False
+    M[curY][curX] = 'O'
+    return True
+def part1():
+    cnt = 0
+    while dropSand(1):
+        cnt += 1
+    print('part1:',cnt)
+    return cnt
+
+def part2(p1):
+    cnt = 0
+    while dropSand(2):
+        cnt += 1
+    print('part2:',cnt + p1)
+
+p1 = part1()
+part2(p1)
