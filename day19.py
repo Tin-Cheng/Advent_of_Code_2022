9dfc96433ba3248366334a454eae53120ac2f066
Tin-Cheng <chenghotin@gmail.com>
2022-12-20 02:44:08 +0800

    fix day19

--- day19.py
+++ day19.py
@@ -1,6 +1,5 @@
 from collections import deque
 import datetime
-#import threading
 
 f = open("day19test.txt", "r")
 #f = open("day19input.txt", "r")
@@ -17,30 +16,26 @@ def CheckBuiltable(blueprint,Robots,Resources):
     maxResource = [False,False]
     #never create if have enough supply
     maxResource[0] = max(max(blueprint[0],blueprint[1]),max(blueprint[2][0],blueprint[3][0])) == Robots[0]
-    maxResource[1] = (blueprint[2][1] == Robots[1]) #or (Resources[1] >= blueprint[2][1] * 2)
+    maxResource[1] = (blueprint[2][1] == Robots[1]) or (Resources[1] > blueprint[2][1] * 2)
 
-    Creatable3 = Resources[0] >= blueprint[3][0] and Resources[2] >= blueprint[3][1]
-    if Creatable3:
+    if Resources[0] >= blueprint[3][0] and Resources[2] >= blueprint[3][1]:
         BuiltRobots.append([0,0,0,1])
         UsedResources.append([blueprint[3][0],0,blueprint[3][1],0])
         return BuiltRobots,UsedResources
 
-    Creatable2 = Resources[0] >= blueprint[2][0] and Resources[1] >= blueprint[2][1]
-    if Creatable2 > 0:
+    if Resources[0] >= blueprint[2][0] and Resources[1] >= blueprint[2][1]:
         BuiltRobots.append([0,0,1,0])
         UsedResources.append([blueprint[2][0],blueprint[2][1],0,0])
         return BuiltRobots,UsedResources
 
     
-    if not maxResource[1]:
-        if Resources[0] // blueprint[1] > 0:
-            BuiltRobots.append([0,1,0,0])
-            UsedResources.append([blueprint[1],0,0,0])
-    if not maxResource[0]:
-        if Resources[0] // blueprint[0] > 0:
-            BuiltRobots.append([1,0,0,0])
-            UsedResources.append([blueprint[0],0,0,0])
-    if len(BuiltRobots) == 0 or Resources[0] < blueprint[0] * 2 or Resources[0] < blueprint[1] * 2:
+    if not maxResource[1] and Resources[0] // blueprint[1] > 0:
+        BuiltRobots.append([0,1,0,0])
+        UsedResources.append([blueprint[1],0,0,0])
+    if not maxResource[0] and Resources[0] // blueprint[0] > 0:
+        BuiltRobots.append([1,0,0,0])
+        UsedResources.append([blueprint[0],0,0,0])
+    if len(BuiltRobots) == 0 or (Resources[0] < blueprint[0] * 2 and Resources[0] < blueprint[1] * 2):
         BuiltRobots.append([0,0,0,0])
         UsedResources.append([0,0,0,0])
     return BuiltRobots,UsedResources
@@ -81,8 +76,8 @@ def Simulation(blueprint,part):
                 CopyResource[i] -= UsedResource[i]
             q.append([time+1,newRobot,CopyResource])
 
-        for i in range(time+1,targetMinutes+1):
-            been[time+i] = max(been.get(time+i,0),Resources[3] + Robots[3] * (i+1))
+        #for i in range(time+1,targetMinutes+1):
+        #    been[time+i] = max(been.get(time+i,0),Resources[3] + Robots[3] * (i+1))
 
 
     print(best,finalRobots,finalResource)
@@ -92,12 +87,10 @@ result1 = 0
 for i,blueprint in enumerate(Blueprints):
     print(datetime.datetime.now(),i,Blueprints[i])
     result1 += (i+1) * Simulation(Blueprints[i],1)
-print(datetime.datetime.now())
-print("part1:",result1)
+print(datetime.datetime.now(),"part1:",result1)
 
 result2 = 1
 for i in range(min(len(Blueprints),3)):
     print(datetime.datetime.now(),i,Blueprints[i])
     result2 *= Simulation(Blueprints[i],2)
-print(datetime.datetime.now())
-print("part2:",result2)
+print(datetime.datetime.now(),"part2:",result2)
